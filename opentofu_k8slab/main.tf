dae756e8dce6ee2629a47ffc3ca9ac5dcc3c6e18
miroslavG-mg <mgospodinov@mail.bg>
2025-02-22 14:51:46 +0200

    fix-sg

--- opentofu_k8slab/main.tf
+++ opentofu_k8slab/main.tf
@@ -80,24 +80,12 @@ resource "aws_security_group" "k8slab_security" {
 resource "aws_vpc_security_group_ingress_rule" "allow_all_port" {
   security_group_id = aws_security_group.k8slab_security.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
-  to_port           = 65535
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_all_icmp" {
-  security_group_id = aws_security_group.k8slab_security.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 8
-  to_port           = 0
-  ip_protocol       = "icmp"
+  ip_protocol       = "-1"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.k8slab_security.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
-  to_port        = 65535  
   ip_protocol       = "-1" 
 }
 
